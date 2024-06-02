16ed7ab53f47405a0a8d50b57d43260967830bdc
DOANCONGPHU <92281800+DOANCONGPHU@users.noreply.github.com>
2024-06-02 21:57:00 +0700

    moi

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// Get elements
 const playButton = document.getElementById('playButton');
 const videoContainer = document.getElementById('videoContainer');
 const myVideo = document.getElementById('myVideo');
 
+// Play video on button click
 playButton.addEventListener('click', () => {
-  videoContainer.style.display = 'block'; // Hiển thị video
-  myVideo.play(); // Bắt đầu phát video
+    videoContainer.style.display = 'block';
+    myVideo.play();
 });
+
